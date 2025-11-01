ffcd2f0d0da71ae5f87e6b5fd26753c6ab88f6d2
sonalsahai2512-spec <sonalsahai2512@gmail.com>
2025-11-02 04:31:12 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,6 @@
+First_Name = input("Enter your first name:") #Asking user to enter his/her first name
+Last_Name = input("Enter your last name:")  #Asking user to enter his/her second name
+
+Full_Name = First_Name + " " + Last_Name #String joining
+
+print("Hello," , Full_Name,"! welcome to the Python program ") #printing the desired output
